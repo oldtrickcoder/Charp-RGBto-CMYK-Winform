9de6ad041e9641e45d1e6b19e85614ca6e0962c6
oldtrickcoder <ypratama66@gmail.com>
2021-12-04 22:34:28 +0700

    Desember2021 development

--- Csharp_rgb_Color/Form1.Designer.cs
+++ Csharp_rgb_Color/Form1.Designer.cs
@@ -38,7 +38,6 @@ namespace Csharp_rgb_Color
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -54,9 +53,9 @@ namespace Csharp_rgb_Color
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.trackBar2 = new System.Windows.Forms.TrackBar();
             this.trackBar3 = new System.Windows.Forms.TrackBar();
-            this.button3 = new System.Windows.Forms.Button();
             this.label16 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
@@ -107,7 +106,7 @@ namespace Csharp_rgb_Color
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(136, 207);
+            this.textBox3.Location = new System.Drawing.Point(136, 226);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(147, 20);
             this.textBox3.TabIndex = 6;
@@ -116,7 +115,7 @@ namespace Csharp_rgb_Color
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(21, 201);
+            this.label3.Location = new System.Drawing.Point(21, 226);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 25);
             this.label3.TabIndex = 5;
@@ -125,24 +124,13 @@ namespace Csharp_rgb_Color
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(136, 291);
+            this.button1.Location = new System.Drawing.Point(136, 303);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 71);
             this.button1.TabIndex = 7;
             this.button1.Text = "CHECK";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(136, 380);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "Convert to CMYK";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label4
             // 
@@ -284,23 +272,12 @@ namespace Csharp_rgb_Color
             // 
             // trackBar3
             // 
-            this.trackBar3.Location = new System.Drawing.Point(136, 233);
+            this.trackBar3.Location = new System.Drawing.Point(136, 252);
             this.trackBar3.Maximum = 255;
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Size = new System.Drawing.Size(147, 45);
             this.trackBar3.TabIndex = 23;
             this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
-            // 
-            // button3
-            // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(136, 415);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(147, 23);
-            this.button3.TabIndex = 24;
-            this.button3.Text = "HEX COLOR CODE";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label16
             // 
@@ -310,7 +287,7 @@ namespace Csharp_rgb_Color
             this.label16.ForeColor = System.Drawing.Color.Black;
             this.label16.Location = new System.Drawing.Point(419, 179);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(318, 52);
+            this.label16.Size = new System.Drawing.Size(317, 52);
             this.label16.TabIndex = 25;
             this.label16.Text = "#HEX-COLOR";
             // 
@@ -325,14 +302,25 @@ namespace Csharp_rgb_Color
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(136, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 29);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Switch";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(804, 452);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label16);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.trackBar3);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
@@ -348,7 +336,6 @@ namespace Csharp_rgb_Color
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
@@ -381,7 +368,6 @@ namespace Csharp_rgb_Color
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
@@ -397,9 +383,9 @@ namespace Csharp_rgb_Color
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.TrackBar trackBar2;
         private System.Windows.Forms.TrackBar trackBar3;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Csharp_rgb_Color/Form1.cs
+++ Csharp_rgb_Color/Form1.cs
@@ -33,19 +33,7 @@ namespace Csharp_rgb_Color
 
         private void button1_Click(object sender, EventArgs e)
         {
-            label16.Visible = false;
-            label15.Visible = false;
-            label14.Visible = false;
-            label13.Visible = false;
-            label12.Visible = false;
-            label11.Visible = false;
-            label10.Visible = false;
-            label9.Visible = false;
-            label8.Visible = false;
-            label7.Visible = false;
-            label6.Visible = false;
-            label5.Visible = false;
-            label4.Visible = false;
+         
             if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0)
             {
                 MessageBox.Show("Mohon isi angkanya terlebih dahulu", "NOTED !!");
@@ -63,6 +51,67 @@ namespace Csharp_rgb_Color
             else
             {
                 pictureBox1.BackColor = Color.FromArgb(x, y, z);
+                double rd = x / 255.000;
+                double grn = y / 255.000;
+                double bleu = z / 255.000;
+
+                double maxwarna;
+                if (rd > grn)
+                {
+
+
+                    if (rd > bleu)
+                    {
+                        maxwarna = rd;
+                    }
+                    else
+                    {
+                        maxwarna = bleu;
+                    }
+
+
+                }
+                else if (grn > bleu)
+                {
+                    maxwarna = grn;
+                }
+                else
+                {
+                    maxwarna = bleu;
+                }
+
+                double cyan, magenta, yellow, k, k_publish;
+                k = (1 - maxwarna);
+                cyan = ((1 - rd - k) / (1 - k)) * 100;
+                magenta = ((1 - grn - k) / (1 - k)) * 100;
+                yellow = ((1 - bleu - k) / (1 - k)) * 100;
+                k_publish = k * 100;
+
+                double cyancode= Math.Round(cyan, 0);
+                double magcode = Math.Round(magenta, 0);
+                double yellowcode=Math.Round(yellow, 0);
+                double kcode=Math.Round(k_publish, 0);
+                Color myColor = Color.FromArgb(x, y, z);
+                string hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+                label16.Text = "#" + hex;
+
+                label15.Visible = true;
+                label14.Visible = true;
+                label13.Visible = true;
+                label12.Visible = true;
+                label11.Visible = true;
+                label10.Visible = true;
+                label9.Visible = true;
+                label8.Visible = true;
+                label7.Visible = true;
+                label6.Visible = true;
+                label5.Visible = true;
+                label4.Visible = true;
+                label16.Visible = true;
+                label4.Text = cyancode.ToString();
+                label5.Text = magcode.ToString();
+                label6.Text = yellowcode.ToString();
+                label7.Text = kcode.ToString();
 
             }
 
@@ -87,75 +136,7 @@ namespace Csharp_rgb_Color
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-            int x, y, z;
-            x = Convert.ToInt32(textBox1.Text);
-            y = Convert.ToInt32(textBox2.Text);
-            z = Convert.ToInt32(textBox3.Text);
-            pictureBox1.BackColor = Color.FromArgb(x, y, z);
-            double rd = x / 255.000;
-            double grn = y / 255.000;
-            double bleu = z / 255.000;
-
-            double maxwarna;
-            if (rd > grn)
-            {
-
-
-                if (rd > bleu)
-                {
-                    maxwarna = rd;
-                }
-                else
-                {
-                    maxwarna = bleu;
-                }
-
-
-            }
-            else if (grn > bleu)
-            {
-                maxwarna = grn;
-            }
-            else
-            {
-                maxwarna = bleu;
-            }
-
-            double cyan, magenta, yellow, k, k_publish;
-            k = (1 - maxwarna);
-            cyan = ((1 - rd - k) / (1 - k)) * 100;
-            magenta = ((1 - grn - k) / (1 - k)) * 100;
-            yellow = ((1 - bleu - k) / (1 - k)) * 100;
-            k_publish = k * 100;
-
-
-            Console.WriteLine(String.Format("{0:0.#}", cyan));
-            Console.WriteLine(String.Format("{0:0.#}", magenta));
-            Console.WriteLine(String.Format("{0:0.#}", yellow));
-            Console.WriteLine(String.Format("{0:0.#}", k_publish));
-            label7.Text = String.Format("{0:0}", k_publish);
-            label6.Text = String.Format("{0:0}", yellow);
-            label5.Text = String.Format("{0:0}", magenta);
-            label4.Text = String.Format("{0:0}", cyan);
-
-            label15.Visible = true;
-            label14.Visible = true;
-            label13.Visible = true;
-            label12.Visible = true;
-            label11.Visible = true;
-            label10.Visible = true;
-            label9.Visible = true;
-            label8.Visible = true;
-            label7.Visible = true;
-            label6.Visible = true;
-            label5.Visible = true;
-            label4.Visible = true;
-            label16.Visible = false;
-
-        }
+     
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
@@ -173,37 +154,21 @@ namespace Csharp_rgb_Color
             textBox3.Text = trackBar3.Value.ToString();
         }
 
-        private void button3_Click(object sender, EventArgs e)
-        {
-           
-            label15.Visible = false;
-            label14.Visible = false;
-            label13.Visible = false;
-            label12.Visible = false;
-            label11.Visible = false;
-            label10.Visible = false;
-            label9.Visible = false;
-            label8.Visible = false;
-            label7.Visible = false;
-            label6.Visible = false;
-            label5.Visible = false;
-            label4.Visible = false;
-            label16.Visible = true;
-            int x, y, z;
-            x = Convert.ToInt32(textBox1.Text);
-            y = Convert.ToInt32(textBox2.Text);
-            z = Convert.ToInt32(textBox3.Text);
-            pictureBox1.BackColor = Color.FromArgb(x, y, z);
-            Color myColor = Color.FromArgb(x, y, z);
-            string hex = myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
-            label16.Text = "#" + hex;
-
-        }
+    
 
         private void button4_Click(object sender, EventArgs e)
         {
             Form2 creator = new Form2();
             creator.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 switchtocmyk=new Form3();
+            switchtocmyk.ShowDialog();
+            this.Close();
+         
+
         }
     }
 }
